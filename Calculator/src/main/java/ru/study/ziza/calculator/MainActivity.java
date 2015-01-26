3e62916f618b5cb2ecaa144e5a728d34510be345
Yurii Loschakov <jurasikmaniac>
2015-01-26 16:10:33 +0600

    waaa

---
+++ Calculator/src/main/java/ru/study/ziza/calculator/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.study.ziza.calculator;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
